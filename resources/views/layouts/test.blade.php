559cffeba05fc040c73ea0fc51dc75fb54f93baf
jane ramos <hjbibaud@gmail.com>
2019-10-18 08:15:27 -0400

    filters, admin, designs

---
+++ resources/views/layouts/test.blade.php
@@ -0,0 +1,19 @@
+//delete product
+public function deleteProduct($id){
+
+$product = Product::find($id);
+
+$exists =  Storage::disk("local")->exists("public/product_images/".$product->image);
+
+//if old image exists
+if($exists){
+//delete it
+Storage::delete('public/product_images/'.$product->image);
+}
+
+
+Product::destroy($id);
+
+return redirect()->route("adminDisplayProducts");
+
+}
